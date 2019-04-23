3a1e5a09b71c1a46df29446febb33595c4af7b90
carinechen <49924274+carinechen@users.noreply.github.com>
2019-04-23 18:52:43 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+//Importando npms
+const fs = require('fs'); 
+const csv = require('fast-csv');
+const csvjson = require('csvjson');
+
+//Lê o arquivo csv
+const stream = fs.createReadStream('input.csv');
+
+//Cria uma variavel 
+var result = [];
+
+//Deixa o código com espaçamento e alinhamento arruamados
+const streamCSV = csv({
+    headers: true
+})
+
+//Onde ocorre a conversão e armazenamento de dados
+.on('data', (data) => result.push (data))
+
+//Faz a saída dos dados em um arquivo JSON
+.on('end', () => {    
+    fs.writeFileSync('output.json', JSON.stringify(result));
+    console.log(result);
+});
+
+stream.pipe(streamCSV);
+
